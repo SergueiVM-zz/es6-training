c556551fb91397060906e4b90cfaf182b3238535
SergueiVM <SergueiVM>
2016-05-30 10:28:50 +0200

    Ejemplo uso nuevas funciones para Strings

---
+++ src/es6/02_stringSamples.js
@@ -0,0 +1,21 @@
+//stringSamples
+function ejemploString(){
+	var msg = "Hello world!";
+	
+	console.log(msg.startsWith("Hello"));
+	console.log(msg.endsWith("!"));
+	console.log(msg.includes("o"));
+	
+	console.log(msg.startsWith("o"));
+	console.log(msg.endsWith("world!"));
+	console.log(msg.includes("x"));
+	
+	console.log(msg.startsWith("o", 4));
+	console.log(msg.endsWith("o", 8));
+	console.log(msg.includes("o", 8))
+}
+
+
+export default function(){
+	ejemploString();
+};
